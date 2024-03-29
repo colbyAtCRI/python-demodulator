171b0dad2ed36440f904f8bd62e556380e658eb1
Paul Colby <paulccolby@earthlink.net>
2024-03-29 09:41:36 -0700

    fixed aliasing with 8'th order IIR lowpass

--- ssb.hpp
+++ ssb.hpp
@@ -8,29 +8,25 @@ class SSBReciever
 {
     bool         mLSB;
     nco_crcf     mShift;
-    CDecimator   mDecim;
     agc_crcf     mAGC;
     firhilbf     mHilbert;
-    firfilt_crcf mLowpass;
-    resamp_rrrf  mAudio;
+    iirfilt_crcf mLowpass;
+    resamp_crcf  mAudio;
 
 public:
 
-    SSBReciever (std::string band, float bandwidth, float iq_rate, float pcm_rate) : mDecim((int)(0.5*iq_rate/pcm_rate),20,60.0f)
+    SSBReciever (std::string band, float bandwidth, float iq_rate, float pcm_rate) 
     {
         mLSB = band == "lsb";
-        // iq_rate -> 2 * pcm_rate (nominal 96000)
-        float irate = iq_rate / mDecim.get_decimation();
-        float arate = pcm_rate / irate; 
         mHilbert = firhilbf_create (55,60.0f);
         mAGC = agc_crcf_create ();
         agc_crcf_set_bandwidth (mAGC, 0.01f);
         agc_crcf_set_scale (mAGC,0.1f);
-        mLowpass = firfilt_crcf_create_kaiser (20, bandwidth/irate, 60.0f, 0.0f);
-        mAudio = resamp_rrrf_create_default (arate);
+        mLowpass = iirfilt_crcf_create_lowpass (8, bandwidth/iq_rate);
+        mAudio = resamp_crcf_create_default (pcm_rate/iq_rate);
         mShift = nco_crcf_create (LIQUID_NCO);
-        float sign = (mLSB) ? -1.0f : 1.0f;
-        nco_crcf_set_frequency (mShift, sign * M_PI * (bandwidth+40.0)/irate);
+        float sign = (mLSB) ? 1.0f : -1.0f;
+        nco_crcf_set_frequency (mShift, sign * M_PI * (bandwidth+40.0f)/iq_rate);
     }
 
    ~SSBReciever (void) 
@@ -38,32 +34,31 @@ public:
         nco_crcf_destroy     (mShift);
         agc_crcf_destroy     (mAGC);
         firhilbf_destroy     (mHilbert);
-        firfilt_crcf_destroy (mLowpass);
-        resamp_rrrf_destroy  (mAudio);
+        iirfilt_crcf_destroy (mLowpass);
+        resamp_crcf_destroy  (mAudio);
     }
 
     array_r execute (array_c inp) 
     {
-        array_c xa = mDecim.execute (inp);
-        complex_t x[py::len(xa)], y[py::len(xa)];
-        float     usb[py::len(xa)], lsb[py::len(xa)];
-        array_to_data<complex_t>(xa,x);
-        for (unsigned int n = 0; n < py::len(xa); n++) {
+        complex_t x[py::len(inp)], y[py::len(inp)];
+        float     usb[py::len(inp)], lsb[py::len(inp)];
+        unsigned int nw;
+        array_to_data<complex_t>(inp,x);
+        // apply lowpass agc and HT to full iq rate
+        for (unsigned int n = 0; n < py::len(inp); n++) {
             nco_crcf_mix_up (mShift, x[n], &x[n]);
-            firfilt_crcf_execute_one (mLowpass, x[n], &x[n]);
-            agc_crcf_execute (mAGC, x[n], &x[n]);
+            iirfilt_crcf_execute (mLowpass, x[n], &x[n]);
             nco_crcf_mix_down (mShift,x[n],&x[n]);
-            firhilbf_c2r_execute (mHilbert, x[n], &lsb[n], &usb[n]);
             nco_crcf_step (mShift);
+            agc_crcf_execute (mAGC, x[n], &x[n]);
         }
+        resamp_crcf_execute_block (mAudio, x, py::len(inp), x, &nw);
+        for (auto n = 0; n < nw; n++)
+            firhilbf_c2r_execute (mHilbert, x[n], &lsb[n], &usb[n]);
         if (mLSB) {
-            unsigned int nw;
-            resamp_rrrf_execute_block (mAudio, lsb, py::len(xa), lsb, &nw);
             return array_from_data<float>(lsb,nw);
         }
         else {
-            unsigned int nw;
-            resamp_rrrf_execute_block (mAudio, usb, py::len(xa), usb, &nw);
             return array_from_data<float>(usb,nw);
         }
     }
